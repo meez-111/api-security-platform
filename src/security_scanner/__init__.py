675d493ec4567b8ef9061761c15ba0cd2787a044
meez <meez.sabra.111@gmail.com>
2025-10-06 18:38:27 +0300

    add config profiles

--- src/security_scanner/__init__.py
+++ src/security_scanner/__init__.py
@@ -1,13 +1,14 @@
 __version__ = "1.0.0"
 
 from .core.models import ScanConfig
-from .core.scanner import SecurityScanner
+from .core.scanner import SecurityScanner, create_security_scanner
 from .core.config_manager import ConfigManager
 from .reporters import HTMLReporter, JSONReporter
 
 __all__ = [
     "ScanConfig",
     "SecurityScanner",
+    "create_security_scanner",
     "ConfigManager",
     "HTMLReporter",
     "JSONReporter",
